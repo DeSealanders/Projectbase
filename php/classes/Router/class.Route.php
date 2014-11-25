4e1d6f0573af9641b0e631b945840b5a11cfa221
Baabah <mail@peterton.nl>
2014-11-25 13:24:14 +0100

    Refactored router

---
+++ php/classes/Router/class.Route.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * Class Route
+ * This class is repsonsible for parsing a route url and determining its destination
+ */
+class Route {
+
+    private $route;
+    private $type;
+    private $matchedRoute;
+    private $imageDetails;
+
+    public function __construct($request, $script) {
+        $this->route = $this->parseUrl($request, $script);
+        $this->matchedRoute = $this->getDestination($this->route);
+    }
+
+    /**
+     * Getter for the type of the route
+     * This can be configured, image, include, page or none
+     * @return mixed a string containing the type of the route
+     */
+    public function getType() {
+        return $this->type;
+    }
+
+    /**
+     * Getter for the route matched
+     * @return bool|string a route if one could be found
+     */
+    public function getMatchedRoute() {
+        return $this->matchedRoute;
+    }
+
+    /**
+     * Getter for details for an image url (width, height and filepath)
+     * @return mixed a list of details for the image specified in the route
+     */
+    public function getImageDetails() {
+        return $this->imageDetails;
+    }
+
+    /**
+     * Convert the request and script url into a parsed url
+     * @param $request the page which was requested
+     * @param $script the page on where this script was called from (index.php usually)
+     * @return string script url substracted from the request url
+     */
+    private function parseUrl($request, $script) {
+
+        // First create arrays from both urls
+        $request = explode('/', $request);
+        $script = explode('/', $script);
+
+        // Then compare them (case-insensitive)
+        $parsedUrl = array_udiff($request, $script, 'strcasecmp');
+
+        // Return the parsed url (with reset index numbers)
+        return array_values($parsedUrl);
+    }
+
+    /**
+     * Match the parsed route by checking for a configured routes, images, includes or pages
+     * @param $route the route to be matched to a page
+     * @return bool|string the route if one could be found, false otherwise
+     */
+    private function getDestination($route) {
+
+        // Create a filepath for the route
+        $routePath = implode('/', $route);
+
+        // First check if the route is configured
+        if($matchedRoute = $this->isConfiguredRoute($routePath)) {
+            $this->type = 'configured';
+
+        }
+        // Secondly check if an image is specified
+        else if($matchedRoute = $this->isImage($route)) {
+            $this->type = 'image';
+        }
+
+        // Thirdly check if route is a CSS or JS inlcude
+        else if($matchedRoute = $this->isInclude($routePath)) {
+            $this->type = 'include';
+        }
+
+        // Otherwise check for a page matching this route
+        else if($matchedRoute = $this->isPage($routePath)) {
+            $this->type = 'page';
+        }
+
+        // Return false if no type could be found
+        else {
+            $this->type = 'none';
+            $matchedRoute = false;
+        }
+
+        // Return the matched route (false if none could be found)
+        return $matchedRoute;
+    }
+
+    /**
+     * Check the conf.router.php file for a matching route
+     * @param $routePath the route to be matched
+     * @return bool|string the configured route if one could be found, false otherwise
+     */
+    private function isConfiguredRoute($routePath) {
+
+        // Go through all configured routes
+        foreach(RouterConfig::getInstance()->getRoutes() as $origin => $destination) {
+
+            // Compare each to the route entered by the user
+            if($origin == $routePath) {
+
+                // Check if the route file actually exists
+                if(file_exists($destination)) {
+                    return $destination;
+                }
+                else {
+                    Logger::getInstance()->writeMessage('Unable to find configured route: ' . $destination);
+                }
+            }
+        }
+
+        // Return false if no configured route could be found
+        return false;
+    }
+
+    /**
+     * Check route for a valid image call
+     * A valid call example: image/200x300/img/democms.jpg
+     * @param $route the route to be matched
+     * @return bool|string the image if one could be found, false otherwise
+     */
+    private function isImage($route) {
+
+        // See if url starts with image
+        if(isset($route[0]) && $route[0] == 'image') {
+
+            // Check for right amount of parameters
+            if(count($route) == 4) {
+
+                // If a width and height can be found
+                $dimensions = explode('x', $route[1]);
+                if(count($dimensions) == 2) {
+
+                    // Set parameters for futher use
+                    $this->imageDetails = array(
+                        'width' => $dimensions[0],
+                        'height' => $dimensions[1],
+                    );
+
+                    // Return image url
+                    return implode('/', array_slice($route, 2));
+                }
+            }
+        }
+
+        // Return false if no image could be found
+        return false;
+    }
+
+    /**
+     * Check the conf.includes.php file for any includes
+     * @param $route the route to be matched
+     * @return bool|string the include if one could be found, false otherwise
+     */
+    private function isInclude($routePath) {
+
+        // Compare includes to the routePath
+        foreach(IncludesConfig::getInstance()->getAllIncludes() as $include) {
+
+            // Return a matched include
+            if (strpos($routePath, $include) !== false) {
+                return $include;
+            }
+        }
+
+        // Return false if no include could be found
+        return false;
+    }
+
+    /**
+     * Check if the requested route is a page in the /php/pages/ folder
+     * @param $routePath the requested route
+     * @return bool|string the route if one could be found, false otherwise
+     */
+    private function isPage($routePath) {
+
+        // Use index.php if route is the root
+        if($routePath == '') {
+            $routePath = 'index.php';
+        }
+
+        // Add .php extension if not present
+        if(pathinfo($routePath, PATHINFO_EXTENSION) == '') {
+            $routePath .= '.php';
+        }
+
+        // See if a page route exists
+        $pageRoute = 'php/pages/' . $routePath;
+        if(file_exists($pageRoute)) {
+            return $pageRoute;
+        }
+
+        // Return false if no file could be found
+        return false;
+
+    }
+
+} 
--- php/classes/Router/class.Router.php
+++ php/classes/Router/class.Router.php
@@ -2,11 +2,8 @@
 
 /**
  * Class Router
- * This class is repsonsible for loading the correct pages
- * It does:
- * 1. Look for alternative routes configured in conf.router.php
- * 2. Redirect all remaining requests to their respective files in /php/pages/
- * 3. Serve up the 404 page if no page could be found
+ * This class is repsonsible for serving up the correct pages
+ *
  * Example:
  *      www.website.com/henkdetank/
  * will be redirected to
@@ -14,16 +11,10 @@
  */
 class Router {
 
-    private $routerConfig;
-    private $includeConfig;
-
-    /**
-     * Load config settings
-     */
     private function __construct() {
-        $this->routerConfig = new RouterConfig();
-        $this->includeConfig = new IncludesConfig();
+
     }
+
     /**
      * Function for creating only 1 instance and return that each time its called (singleton)
      * @return Router
@@ -44,66 +35,11 @@ class Router {
      */
     public function route($request, $script) {
 
-        // First pares the url
-        $route = $this->parseUrl($request, $script);
+        // Create a router object
+        $route = new Route($request, $script);
 
-        // Then check if a route could be matched (from config or default page settings)
-        $matchedRoute = $this->matchRoute($route);
-
-        // Then load the found route
-        $this->loadRoute($matchedRoute);
-
-    }
-
-    /**
-     * Convert the request and script url into a parsed url
-     * @param $request the page which was requested
-     * @param $script the page on where this script was called from (index.php usually)
-     * @return string script url substracted from the request url
-     */
-    private function parseUrl($request, $script) {
-
-        // First create arrays from both urls
-        $request = explode('/', $request);
-        $script = explode('/', $script);
-
-        // Then compare them (case-insensitive)
-        $parsedUrl = array_udiff($request, $script, 'strcasecmp');
-
-        // When no directory is found, send to root
-        if(empty($parsedUrl)) {
-            $parsedUrl[0] = 'index';
-        }
-
-        // Return the remainder of the url
-        return $parsedUrl;
-    }
-
-    /**
-     * Match the parsed route to the configured routes
-     * If none could be found check the default routes
-     * @param $route the route to be matched to a page
-     * @return bool|string the route if one could be found, false otherwise
-     */
-    private function matchRoute($url) {
-
-        // Check for configured routes first
-        if(!($matchedRoute = $this->isConfiguredRoute(implode('/', $url)))) {
-
-            // Check to see if this is an JS or CSS include
-            if(!($matchedRoute = $this->isIncludeRoute($url))) {
-
-                // Use default route if no configured routes have been found
-                if(!($matchedRoute = $this->isDefaultRoute(implode('/', $url)))) {
-
-                    // No route could be found
-                    $matchedRoute = false;
-
-                }
-            }
-
-        }
-        return $matchedRoute;
+        // Load the correct route by giving it the route
+        $this->loadRoute($route);
     }
 
     /**
@@ -112,102 +48,68 @@ class Router {
      */
     private function loadRoute($route) {
 
-        // Do include route in default page if configured so
-        $showRaw = false;
-
-        // Retrieve file extension
-        $extension = pathinfo($route, PATHINFO_EXTENSION);
-
-        // Set proper content types for css
-        if($extension == 'css') {
-            header("Content-type: text/css");
-            $showRaw = true;
+        // Load a CSS or JS include
+        if($route->getType() == 'include') {
+            $this->loadIncludeRoute($route->getMatchedRoute());
         }
 
-        // Set proper content types for js
-        else if($extension == 'js') {
-            header('Content-Type: application/javascript');
-            $showRaw = true;
+        // Load an image
+        else if($route->getType() == 'image') {
+
+            // Let the image loader handle the rest of this request
+            ImageLoader::getInstance()->loadImage($route->getMatchedRoute(), $route->getImageDetails());
         }
 
-        // Load the default page if it is set
-        if($route && !$showRaw && $defaultPage = $this->routerConfig->getDefaultPage()) {
-
-            // Set the route so it can be used in the default page
-            $_SERVER['ROUTE'] = $route;
-            require($defaultPage);
-        }
-
-        // Load the found route without a default page
-        else if($route) {
-            require($route);
+        // Load a configured or regular page
+        else if($route->getType() == 'configured' || $route->getType() == 'page') {
+            $this->loadPageRoute($route->getMatchedRoute());
         }
 
         // If no route could be found, serve up the 404 page
-        else {
+        else if($route->getType() == 'none') {
             require 'php/pages/404.php';
         }
     }
 
     /**
-     * Check the conf.router.php file for a matching route
-     * @param $route the route to be matched
-     * @return bool|string the route if one could be found, false otherwise
+     * Load a CSS or JS route
+     * @param $matchedRoute the route to be loaded
      */
-    private function isConfiguredRoute($route) {
-        $matchedRoute = false;
+    private function loadIncludeRoute($matchedRoute) {
+        $extension = pathinfo($matchedRoute, PATHINFO_EXTENSION);
 
-        // Go through the configured routes
-        $confRoutes = $this->routerConfig->getRoutes();
-        foreach($confRoutes as $origin => $destination) {
-
-            // Compare each to the route entered by the user
-            if($origin == $route) {
-                $matchedRoute = $destination;
-            }
+        // Set proper content types for css
+        if($extension == 'css') {
+            header("Content-type: text/css");
         }
 
-        return $matchedRoute;
+        // Set proper content types for js
+        else if($extension == 'js') {
+            header('Content-Type: application/javascript');
+        }
+
+        // Load the css or js file
+        require($matchedRoute);
+
     }
 
     /**
-     * Check the conf.includes.php file for any includes
-     * @param $route the route to be matched
-     * @return bool|string the include if one could be found, false otherwise
+     * Load a page and check if a wrapper should be used
+     * @param $matchedRoute
      */
-    private function isIncludeRoute($route) {
+    private function loadPageRoute($matchedRoute) {
 
-        // Load all configured includes
-        $confIncludes = array_merge($this->includeConfig->getCssIncludes(), $this->includeConfig->getJsIncludes());
+        // Load the default page if it is set
+        if($wrapperPage = RouterConfig::getInstance()->getWrapperPage()) {
 
-        // See if an include matches the route
-        foreach($confIncludes as $include) {
-            if (strpos(implode('/', $route), $include) !== false) {
-                return $include;
-            }
-        }
-        return false;
-    }
-
-    /**
-     * Check if the requested route is a page in the /php/pages/ folder
-     * @param $route the requested route
-     * @return bool|string the route if one could be found, false otherwise
-     */
-    private function isDefaultRoute($route) {
-
-        // Add .php extension if not present
-        if(pathinfo($route, PATHINFO_EXTENSION) == '') {
-            $route .= '.php';
+            // Set the route so it can be used in the wrapper page
+            $_SERVER['ROUTE'] = $matchedRoute;
+            require($wrapperPage);
         }
 
-        // See if default route exists
-        $defaultRoute = 'php/pages/' . $route;
-        if(file_exists($defaultRoute)) {
-            return $defaultRoute;
-        }
+        // Load the found route without a default page
         else {
-            return false;
+            require($matchedRoute);
         }
     }
 
