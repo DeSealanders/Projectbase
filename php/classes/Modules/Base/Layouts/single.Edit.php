adda0c58a2b5cd310edad8be89c3051531814683
Baabah <mail@peterton.nl>
2014-12-13 19:58:54 +0100

    Only show single view if record is present

--- php/classes/Modules/Base/Layouts/single.Edit.php
+++ php/classes/Modules/Base/Layouts/single.Edit.php
@@ -7,22 +7,29 @@ $record = $this->module->getRecord($itemid);
 // Create a form for all module components
 $form = new Form($this->module->getName(), 'edit');
 
-// Add all components to the form
-foreach($this->module->getComponents() as $component) {
-    // Loop through all record fields
-    foreach($record as $field => $value) {
+if($record) {
 
-        // Only add components if they are present in the database record
-        if($field == strtolower($component->getId())) {
+    // Add all components to the form
+    foreach($this->module->getComponents() as $component) {
+        // Loop through all record fields
+        foreach($record as $field => $value) {
 
-            // Add a formcomponent to the form
-            $form->addComponent($component->getFormCompontent($value));
+            // Only add components if they are present in the database record
+            if($field == strtolower($component->getId())) {
+
+                // Add a formcomponent to the form
+                $form->addComponent($component->getFormCompontent($value));
+            }
         }
     }
-}
 
-// Add a save button
-$form->addComponent(new Button('Save', 'save'));
+    // Add a save button
+    $form->addComponent(new Button('Save', 'save'));
+
+}
+else {
+    $form->addComponent(new CustomHtml('error', '<p>This item could not be found</p>'));
+}
 
 // Show the form
 $form->printHtml();
