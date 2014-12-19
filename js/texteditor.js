8ea2ed00abd49f03f460231db8f0b3020473281f
Baabah <mail@peterton.nl>
2014-12-19 17:44:11 +0100

    Removed save button from htmleditor

--- js/texteditor.js
+++ js/texteditor.js
@@ -6,18 +6,6 @@ $(function() {
             ["h2", "h3", "h4", "p"],
             ["link", "unlink"],
             ["html"],
-            [{
-                // This is how to add a completely custom Toolbar Button
-                css: "custom_disk_button",
-                text: "Save",
-                action: function(btn) {
-                    var id = this.getTextareaId();
-                    var data = this.toHtmlString();
-                    alert('Save button pressed');
-                    console.log(id);
-                    console.log(data);
-                }
-            }]
         ],
 
     });
