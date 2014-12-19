5d614a97eb0faac14fa13b63c7405a5bf14b4b2b
Baabah <mail@peterton.nl>
2014-12-19 17:56:42 +0100

    Switched text with color components in color module

--- php/classes/Modules/Colors/class.ModuleColors.php
+++ php/classes/Modules/Colors/class.ModuleColors.php
@@ -5,10 +5,10 @@ class ModuleColors extends Module {
     public function __construct() {
         parent::__construct('Colors');
         $this->addLayout('multi');
-        $this->addComponent(new TextComponent('Background color 1', 'backgroundcolor1'));
-        $this->addComponent(new TextComponent('Background color 2', 'backgroundcolor2'));
-        $this->addComponent(new TextComponent('Text color 1', 'textcolor1'));
-        $this->addComponent(new TextComponent('Text color 2', 'textcolor2'));
+        $this->addComponent(new ColorpickerComponent('Background color 1', 'backgroundcolor1'));
+        $this->addComponent(new ColorpickerComponent('Background color 2', 'backgroundcolor2'));
+        $this->addComponent(new ColorpickerComponent('Text color 1', 'textcolor1'));
+        $this->addComponent(new ColorpickerComponent('Text color 2', 'textcolor2'));
         $this->allowNew(false);
         $this->allowDelete(false);
     }
