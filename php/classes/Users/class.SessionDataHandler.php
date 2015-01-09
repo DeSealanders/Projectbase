e2fa2e4629e382a37964eea95edb9a75f6cdc34d
Baabah <mail@peterton.nl>
2015-01-09 23:04:41 +0100

    Added sessiondatahandler concept

---
+++ php/classes/Users/class.SessionDataHandler.php
@@ -0,0 +1,25 @@
+<?php
+
+class SessionDataHandler extends Singleton {
+
+    protected function __construct() {
+
+    }
+
+    public function session_start() {
+
+    }
+
+    public function session_regenerate() {
+
+    }
+
+    public function session_destory() {
+
+    }
+
+    private function generateSessionId() {
+        // Nodig?
+    }
+
+} 
