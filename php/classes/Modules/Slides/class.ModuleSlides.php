47a6fc1551ba1ed0aedf1a789d241059f2006b89
Baabah <mail@peterton.nl>
2015-01-10 00:00:33 +0100

    Created more options for the slide module

--- php/classes/Modules/Slides/class.ModuleSlides.php
+++ php/classes/Modules/Slides/class.ModuleSlides.php
@@ -6,10 +6,14 @@ class ModuleSlides extends Module {
         parent::__construct('Slides');
         $this->setDescription('Manage slides which will form a slideshow');
         $this->addLayout('multi');
-        $this->addComponent(new TextComponent('Title', 'name'));
+        $this->addComponent(new TextComponent('Title', 'title'));
         $this->addComponent(new EditorComponent('Content', 'content', false));
         $this->addComponent(new TextComponent('X-coordinate', 'xpos'));
         $this->addComponent(new TextComponent('Y-coordinate', 'ypos'));
+        $this->addComponent(new TextComponent('Z-coordinate', 'zpos'));
+        $this->addComponent(new TextComponent('Size', 'size', true, '1'));
+        // Multi component for rotation (x, y and z) + numberbox
+        // Numberboxes for coördinates and size
     }
 
 } 
