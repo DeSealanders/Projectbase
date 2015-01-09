a3cf824e9953709f9c9f38e6d0e58ad1a4caffbb
Baabah <mail@peterton.nl>
2015-01-10 00:11:25 +0100

    Added more components to slide module

--- php/classes/Modules/Slides/class.ModuleSlides.php
+++ php/classes/Modules/Slides/class.ModuleSlides.php
@@ -7,11 +7,14 @@ class ModuleSlides extends Module {
         $this->setDescription('Manage slides which will form a slideshow');
         $this->addLayout('multi');
         $this->addComponent(new TextComponent('Title', 'title'));
-        $this->addComponent(new EditorComponent('Content', 'content', false));
-        $this->addComponent(new TextComponent('X-coordinate', 'xpos'));
-        $this->addComponent(new TextComponent('Y-coordinate', 'ypos'));
-        $this->addComponent(new TextComponent('Z-coordinate', 'zpos'));
-        $this->addComponent(new TextComponent('Size', 'size', true, '1'));
+        $this->addComponent(new EditorComponent('Content', 'content'));
+        $this->addComponent(new TextComponent('X-coordinate', 'xpos', false));
+        $this->addComponent(new TextComponent('Y-coordinate', 'ypos', false));
+        $this->addComponent(new TextComponent('Z-coordinate', 'zpos', false));
+        $this->addComponent(new TextComponent('Size', 'size', false, '1'));
+        $this->addComponent(new TextComponent('X-rotation', 'xrot', false));
+        $this->addComponent(new TextComponent('Y-rotation', 'yrot', false));
+        $this->addComponent(new TextComponent('Z-rotation', 'zrot', false));
         // Multi component for rotation (x, y and z) + numberbox
         // Numberboxes for coördinates and size
     }
