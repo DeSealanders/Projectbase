339a3e0f0c11a4bb1b8c583126390872bb49b129
Baabah <mail@peterton.nl>
2015-01-09 23:05:04 +0100

    Added todo for sql queries

--- index.php
+++ index.php
@@ -1,9 +1,24 @@
 <?php
-session_start();
+
+
 
 // Load default config
 require_once('php/config/conf.default.php');
 DefaultConfig::getInstance()->init();
+
+session_name('id');
+session_start();
+
+$injection = "Jansen' OR 'a' = 'a";
+$query = new Query();
+$query->select('*');
+$query->from('users');
+$query->where('password = ' . "'" . $injection . "'");
+echo $query;
+
+// Todo fix!
+var_dump(DatabaseManager::getInstance()->executeQuery($query));
+var_dump(DatabaseManager::getInstance()->executeQuery("SELECT * FROM users WHERE password = ?", array($injection)));
 
 // Route the incoming request via the router
 Router::getInstance()->route($_SERVER['REQUEST_URI'], $_SERVER['SCRIPT_NAME']);
