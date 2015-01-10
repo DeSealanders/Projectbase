cf07bd6f0dceffc645cf7322648a7fc388c5d5d6
Baabah <mail@peterton.nl>
2015-01-10 14:45:44 +0100

    Switched some modulecomponents in slider

--- php/classes/Modules/Slides/class.ModuleSlides.php
+++ php/classes/Modules/Slides/class.ModuleSlides.php
@@ -8,15 +8,13 @@ class ModuleSlides extends Module {
         $this->addLayout('multi');
         $this->addComponent(new TextComponent('Title', 'title'));
         $this->addComponent(new EditorComponent('Content', 'content'));
+        $this->addComponent(new NumberSliderComponent('Size', 'size', 1, 10, false));
         $this->addComponent(new TextComponent('X-coordinate', 'xpos', false));
         $this->addComponent(new TextComponent('Y-coordinate', 'ypos', false));
         $this->addComponent(new TextComponent('Z-coordinate', 'zpos', false));
-        $this->addComponent(new TextComponent('Size', 'size', false, '1'));
-        $this->addComponent(new TextComponent('X-rotation', 'xrot', false));
-        $this->addComponent(new TextComponent('Y-rotation', 'yrot', false));
-        $this->addComponent(new TextComponent('Z-rotation', 'zrot', false));
-        // Multi component for rotation (x, y and z) + numberbox
-        // Numberboxes for coördinates and size
+        $this->addComponent(new NumberSliderComponent('X-rotation', 'xrot', 0, 360, 45, false));
+        $this->addComponent(new NumberSliderComponent('Y-rotation', 'yrot', 0, 360, 45, false));
+        $this->addComponent(new NumberSliderComponent('Z-rotation', 'zrot', 0, 360, 45, false));
     }
 
 } 
