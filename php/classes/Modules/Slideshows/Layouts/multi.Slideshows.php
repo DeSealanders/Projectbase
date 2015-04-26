010d4baacd50a1ba701cbb3ad99ea81959b913c0
Baabah <mail@peterton.nl>
2015-04-26 15:32:13 +0200

    Added image to view

--- php/classes/Modules/Slideshows/Layouts/multi.Slideshows.php
+++ php/classes/Modules/Slideshows/Layouts/multi.Slideshows.php
@@ -3,6 +3,9 @@
 $records = $this->getRecords();
 foreach($records as $record) {
     echo '<div class="block">';
+    if(isset($record['image'])) {
+        echo '<img src="' . $record['image'] . '">';
+    }
     echo '<h1>' . $record['title'] . '</h1>';
     echo '<a href="slides?show=' . $record['itemid'] . '">Link</a>';
     echo '</div>';
