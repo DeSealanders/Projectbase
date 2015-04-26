5314b99f2be0ff3cc8a40c082d3f88b861db22ef
Baabah <mail@peterton.nl>
2015-04-26 14:51:16 +0200

    Fixed link to slideshows in overview

--- php/classes/Modules/Slideshows/Layouts/multi.Slideshows.php
+++ php/classes/Modules/Slideshows/Layouts/multi.Slideshows.php
@@ -4,6 +4,6 @@ $records = $this->getRecords();
 foreach($records as $record) {
     echo '<div class="block">';
     echo '<h1>' . $record['title'] . '</h1>';
-    echo '<a href="impressentation/slides?show=' . $record['itemid'] . '">Link</a>';
+    echo '<a href="slides?show=' . $record['itemid'] . '">Link</a>';
     echo '</div>';
 }
