3b7ed33e49e682176c20f7a8fef82e02772437dc
Baabah <mail@peterton.nl>
2015-04-27 11:12:34 +0200

    Added return to overview slide at end of each show

--- php/classes/Modules/Slideshows/class.SlideGenerator.php
+++ php/classes/Modules/Slideshows/class.SlideGenerator.php
@@ -13,6 +13,13 @@ class SlideGenerator extends Singleton {
                 $slidelist[] = new Slide($slide);
             }
         }
+        $slidelist[] = new Slide(
+            array(
+                'title' => 'Naar overzicht',
+                'content' => '<a href="/"><i class="fa fa-link"></i> Klik <b>hier</b> om terug naar het overzicht te gaan</a>',
+                'zpos' => -20000,
+            )
+        );
         return $slidelist;
     }
 
