d473ef9383a722dfb2f0597e6698212d8ff05257
Baabah <mail@peterton.nl>
2015-05-02 11:40:12 +0200

    Removed debug statement

--- js/slideshow-controller.js
+++ js/slideshow-controller.js
@@ -1,7 +1,6 @@
 angular.module('Impress', []).
     controller('slideshowController', function($scope, $http) {
 
-        console.log('test');
         // Load scenarios from the back-end API
         $http.get("api")
             .success(function (response) {
